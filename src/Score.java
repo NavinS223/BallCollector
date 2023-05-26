59aa4cb3db193943a9e7c6cca6292754ea253aaf
1861340 <1861340@01-19956.pusd.dom>
2023-05-26 10:42:56 -0700

    h

---
+++ src/Score.java
@@ -0,0 +1,40 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import java.awt.geom.AffineTransform;
+
+public class Score {
+	private AffineTransform tx;
+	private int score;
+	private int x=400;
+	private int y=400;
+	Circle c = new Circle((int)(Math.random()*401) + 100, -100);
+	hoop h = new hoop();
+	
+	public Score(){
+		tx = AffineTransform.getTranslateInstance(0, 0);
+		init(0,0);
+		score =0;
+	
+		
+		
+	}
+	
+	private void init(double i, double j) {
+		tx.setToTranslation(i, j);// TODO Auto-generated method stub
+		tx.scale(.5, .5);
+	}
+
+public void paint(Graphics g) {
+	Graphics2D g2 =(Graphics2D)g;
+	
+	g2.drawString(score + " ", x, y);
+	init(x,y);
+	if(Rectangle.intersects(h.getRect(), c.getRect())) {
+		score++;
+	}
+
+
+	}
+}
+
